8be4fea88c9393d20e8f1ba84ac6b45b05a5ace6
asitdas2017 <kuarasitdas@gmail.com>
2017-10-20 11:23:57 -0400

    modified chagnes

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -20,7 +20,55 @@ export class NewsPage {
 }
 
 @Component({
-  templateUrl: 'news-details.html'
+  template: `
+  <ion-header>
+  <ion-toolbar>
+      <ion-title>
+          Details
+      </ion-title>
+      <ion-buttons start>
+          <button ion-button (click)="dismiss()">
+            <span ion-text showWhen="ios">Back</span>
+            <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
+          </button>
+      </ion-buttons>
+  </ion-toolbar>
+</ion-header>
+<ion-content>
+
+
+  <ion-card class="news-details">
+      <div class="details-img"><img src="{{character.image}}"></div>
+      <h6>{{character.newsTitle}}</h6>
+      <ion-item>
+          <span item-left><span class="green">CNBC News</span> - 1h ago</span>
+          <button ion-button clear item-end>
+              <ion-icon name="bookmark"></ion-icon>                  
+          </button>
+          <button ion-button clear item-end>
+              <ion-icon name="cloud-download"></ion-icon>                        
+          </button>
+          <button ion-button clear item-end>
+              <ion-icon name="share-alt"></ion-icon>                        
+          </button>
+      </ion-item>
+      <div class="details-copy">
+          <p>Lorem ipsum dolor sit amet, autem epicuri expetenda ei est, ad mei alia soluta, pro an quem regione meliore. Mundi fabulas similique sit cu, an summo consul eos, eu consulatu reprimique conclusionemque eos. Agam legimus inermis sed no, eam
+              etiam graeci appareat no, usu et labitur accusamus accommodare. Odio dolorum no sit, id mea tantas omittam moderatius, atqui splendide ea sea. At nisl habemus iudicabit mea, ex iusto aliquam accusata sea. Cu postea alterum vix, ea mea
+              veri justo. Mei cu causae forensibus adipiscing, pri option suscipit et, ridens oporteat ei his. Per in iuvaret dignissim, ei homero cetero ceteros vix, agam accusamus cu usu. Ne doming percipit vim, ullum summo petentium et nam. Ex debet
+              doctus audire mei. At fabulas pericula interesset pro, in pri sale nulla accommodare.
+              <br><br> Lorem ipsum dolor sit amet, autem epicuri expetenda ei est, ad mei alia soluta, pro an quem regione meliore. Mundi fabulas similique sit cu, an summo consul eos, eu consulatu reprimique conclusionemque eos. Agam legimus inermis
+              sed no, eam etiam graeci appareat no, usu et labitur accusamus accommodare. Odio dolorum no sit, id mea tantas omittam moderatius, atqui splendide ea sea. At nisl habemus iudicabit mea, ex iusto aliquam accusata sea. Cu postea alterum
+              vix, ea mea veri justo. Mei cu causae forensibus adipiscing, pri option suscipit et, ridens oporteat ei his. Per in iuvaret dignissim, ei homero cetero ceteros vix, agam accusamus cu usu. Ne doming percipit vim, ullum summo petentium et
+              nam. Ex debet doctus audire mei. At fabulas pericula interesset pro, in pri sale nulla accommodare.
+              <br><br> Lorem ipsum dolor sit amet, autem epicuri expetenda ei est, ad mei alia soluta, pro an quem regione meliore. Mundi fabulas similique sit cu, an summo consul eos, eu consulatu reprimique conclusionemque eos. Agam legimus inermis
+              sed no, eam etiam graeci appareat no, usu et labitur accusamus accommodare. Odio dolorum no sit, id mea tantas omittam moderatius, atqui splendide ea sea. At nisl habemus iudicabit mea, ex iusto aliquam accusata sea. Cu postea alterum
+              vix, ea mea veri justo. Mei cu causae forensibus adipiscing, pri option suscipit et, ridens oporteat ei his. Per in iuvaret dignissim, ei homero cetero ceteros vix, agam accusamus cu usu. Ne doming percipit vim, ullum summo petentium et
+              nam. Ex debet doctus audire mei. At fabulas pericula interesset pro, in pri sale nulla accommodare.</p>
+      </div>
+  </ion-card>
+</ion-content>
+  `
 })
 export class ModalContentPage {
   character;
